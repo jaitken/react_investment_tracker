aa9c39f7608651b3b57169bf52ccb0d661aad914
jaitken <jaitken97@gmail.com>
2024-10-09 18:20:16 -0400

    added error handling for years <= 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import Header from "./components/Header"
-import UserInput from "./components/UserInput"
-import { useState } from "react"
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import { useState } from "react";
 import Results from "./components/Results";
 
-
 function App() {
-  
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1000,
     expectedReturn: 6,
-    duration: 10
-});
+    duration: 10,
+  });
 
-function handleChange(inputIdentifier, newValue){
-  setUserInput(prevUserInput => {
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
       return {
-          ...prevUserInput,
-          [inputIdentifier]: +newValue
-      }
-  })
-}
-  
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
-    <Header/>
-    <UserInput onChange={handleChange} userInput={userInput}/>
-    <Results input={userInput}/>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero </p>}
+      { inputIsValid && <Results input={userInput} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
